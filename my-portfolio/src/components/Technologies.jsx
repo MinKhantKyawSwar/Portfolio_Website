f00281c7fd034a6b13cec5a5c3f7159720b9621f
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2024-12-05 08:02:16 -0800

    final portfolio

--- my-portfolio/src/components/Technologies.jsx
+++ my-portfolio/src/components/Technologies.jsx
@@ -30,21 +30,21 @@ const Technologies = () => {
             whileInView={{opacity: 1, x:0}}
             initial={{opacity:0, x:-100}}
             transition={{duration:2}}
-            className='flex flex-warp items-center justify-center lg:gap-20 gap-6'>
+            className='flex flex-warp items-center justify-center lg:gap-20 gap-4'>
                 <motion.div variants={iconVarients(2.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <SiTailwindcss className="text-7xl text-cyan-300"/>
+                    <SiTailwindcss className="text-5xl lg:text-7xl text-cyan-300"/>
                 </motion.div>
                 <motion.div variants={iconVarients(3)} initial="initial" animate="animate"  className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <RiJavascriptFill className="text-7xl text-yellow-400"/>
+                    <RiJavascriptFill className="text-5xl lg:text-7xl text-yellow-400"/>
                 </motion.div>
                 <motion.div  variants={iconVarients(2)} initial="initial" animate="animate"  className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <RiJavaFill className="text-7xl text-neutral-50"/>
+                    <RiJavaFill className="text-5xl lg:text-7xl text-neutral-50"/>
                 </motion.div>
                 <motion.div variants={iconVarients(6)} initial="initial" animate="animate"  className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <FaPhp className="text-7xl text-blue-700"/>
+                    <FaPhp className="text-5xl lg:text-7xl text-blue-700"/>
                 </motion.div>
                 <motion.div variants={iconVarients(4)} initial="initial" animate="animate"  className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <AiOutlinePython className="text-7xl text-white"/>
+                    <AiOutlinePython className="text-5xl lg:text-7xl text-white"/>
                 </motion.div> 
                 
             </motion.div>
@@ -52,21 +52,21 @@ const Technologies = () => {
             whileInView={{opacity: 1, x:0}}
             initial={{opacity:0, x:100}}
             transition={{duration:2}}
-            className='flex flex-warp items-center justify-center lg:gap-20 gap-6 mt-10 '>
+            className='flex flex-warp items-center justify-center lg:gap-20 gap-4 mt-10 '>
                 <motion.div  variants={iconVarients(4)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <RiReactjsLine className="text-7xl text-cyan-400"/>
+                    <RiReactjsLine className="text-5xl lg:text-7xl text-cyan-400"/>
                 </motion.div>
                 <motion.div  variants={iconVarients(2.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <FaNodeJs className="text-7xl text-green-400"/>
+                    <FaNodeJs className="text-5xl lg:text-7xl text-green-400"/>
                 </motion.div>
                 <motion.div  variants={iconVarients(1.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <SiExpress className="text-7xl text-white"/>
+                    <SiExpress className="text-5xl lg:text-7xl text-white"/>
                 </motion.div>
                 <motion.div  variants={iconVarients(5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <SiMongodb className="text-7xl text-green-500"/>
+                    <SiMongodb className="text-5xl lg:text-7xl text-green-500"/>
                 </motion.div>
                 <motion.div  variants={iconVarients(1.5)} initial="initial" animate="animate" className='rounded-2xl border-4 border-neutral-800 p-'>
-                    <GrMysql className="text-7xl text-blue-500"/>
+                    <GrMysql className="text-5xl lg:text-7xl text-blue-500"/>
                 </motion.div>
             </motion.div>
         </motion.div>
