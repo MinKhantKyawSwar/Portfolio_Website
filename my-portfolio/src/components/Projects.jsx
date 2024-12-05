29357cd99bb48a0f0138df39d8553675277a48cf
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2024-12-05 07:52:05 -0800

    final portfolio

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -23,14 +23,14 @@ const Projects = () => {
                         alt={project.title} 
                         width={250} 
                         height={150} 
-                        className='mb-6 rounded'/>
+                        className='mb-6 rounded sm:w-full'/>
                         </a>
                     </div>
                     <motion.div
                     whileInView={{opacity: 1, x:0}}
                     initial={{opacity:0, x:100}}
                     transition={{duration:1}} 
-                    className='w-full max-w-xl lg:w-3/4'>
+                    className='w-full max-w-xl lg:w-3/4 lg:ml-20'>
                         <h6 className="mb-2 font-semibold">
                             <a href={project.demoLink} target="_blank" rel="noopener noreferrer" className="text-purple-500 hover:underline">
                                 {project.title}
